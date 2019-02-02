03d9b5db0db0da4265fe44da49786c82d60bc06a
Smushed <smushedcode@gmail.com>
2019-02-02 13:49:36 -0600

    Troubleshooting Heroku

--- client/src/Router.js
+++
@@ -1,6 +0,0 @@
-export default <Router history={hashHistory}>
-    <Route path="/" component={App}>
-        <IndexRoute component={AppSplash} />
-        <Route path="demo" component={AppDemo} />
-    </Route>
-</Router>
