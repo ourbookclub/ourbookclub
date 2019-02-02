89dd273a7e956e933ee69f89f6925a8eae8c6144
Smushed <smushedcode@gmail.com>
2019-02-02 13:02:42 -0600

    Initial Commit with React

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+class App extends Component {
+
+  state = {
+    "test": "is a test",
+    "result": ""
+  }
+
+  componentDidMount() {
+    console.log("Mounted")
+    axios.get("/api/test", (result) => {
+      this.setState({ "result": result })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          Test value {this.state.test} <br />
+          {this.state.result}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
