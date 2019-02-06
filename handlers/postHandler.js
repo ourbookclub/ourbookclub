233ae59a6188c6a3d6e7d11668900184ae6f9e39
Smushed <smushedcode@gmail.com>
2019-02-05 19:58:53 -0600

    Post can be added and grabbed

--- handlers/postHandler.js
+++ handlers/postHandler.js
@@ -2,6 +2,26 @@ const db = require(`../models`);
 
 module.exports = {
     createPost: async (userID, groupID, userPost) => {
-        console.log(userID, groupID, userPost)
+
+        const newPost = {
+            user: userID,
+            group: groupID,
+            date: Date.now(),
+            title: userPost.title,
+            text: userPost.text,
+            isSpoiler: userPost.isSpoiler
+        };
+
+        const addedPost = await db.Post.create(newPost);
+
+        return addedPost;
+    },
+    getGroupPost: async (groupID) => {
+        const groupPosts = await db.Post.find({ group: groupID });
+        return groupPosts;
+    },
+    sortPostByDate: async (groupPosts) => {
+        const sortedPosts = groupPosts.sort((a, b) => b.date - a.date);
+        return sortedPosts;
     }
 }
