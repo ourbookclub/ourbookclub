2ec11fc95e6ab4c0ae5f3a2f2f6ad463091a5c5e
Smushed <smushedcode@gmail.com>
2019-02-07 19:51:43 -0600

    Books now adding to DB

--- handlers/bookHandler.js
+++ handlers/bookHandler.js
@@ -1,4 +1,5 @@
 const axios = require(`axios`);
+const db = require(`../models`);
 
 const checkForISBN = (industryIdentifiers) => {
     //Goes through the data that google returned and searches if the book has an ISBN number tied to it
@@ -14,12 +15,29 @@ const checkForISBN = (industryIdentifiers) => {
     }
 };
 
+const checkDuplicate = async (bookTitle) => {
+    try {
+        const searchedBook = await db.Book.findOne({ title: bookTitle });
+        //If there is a group with that name return true
+        if (searchedBook !== null) {
+            return true
+        };
+    } catch (err) {
+        //TODO Something to show the error
+        console.log(err);
+    };
+};
+
 module.exports = {
     //All functions and files are singular on the back end
     //When searching google books spaces need to be turned into +
     searchGoogleBook: async (searchedBook) => {
-        const search = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchedBook}&maxResults=20&printType=Books`);
-
+        try {
+            const search = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchedBook}&maxResults=20&printType=Books`);
+        } catch (err) {
+            //TODO Add an error message here
+            return "Something went wrong"
+        }
 
         //TODO Try and do this in one ForEach or something
         const searchedBookArray = await search.data.items.filter(book => checkForISBN(book.volumeInfo.industryIdentifiers));
@@ -44,6 +62,24 @@ module.exports = {
         return returnedBookList;
     },
     saveBookToDB: async (savedBook) => {
-
+        //If the book is not a duplicate then save it to the database
+        if (!checkDuplicate(savedBook.title)) {
+            try {
+                const newBook = await db.Book.create(savedBook);
+                return newBook;
+            } catch (err) {
+                //TODO Add an error message here
+                return err;
+            };
+        } else {
+            //If the book is a duplicate, then get the book and return it to the user as if it was saved so they can then add it to the group
+            try {
+                const foundBook = await db.Book.findOne({ title: savedBook.title });
+                return foundBook;
+            } catch (err) {
+                //TODO Add an error message here
+                return err
+            }
+        };
     }
 };
