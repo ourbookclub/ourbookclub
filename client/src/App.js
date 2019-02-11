d4c34c21086a231a205df017e89ab9ab07d1ea0e
william mcclung <wpmcclung@gmail.com>
2019-02-11 02:06:07 -0600

    HTML Skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,8 @@ import axios from "axios";
 class App extends Component {
 
   state = {
-    "test": "is a test",
-    "result": ""
+    // "test": "is a test",
+    // "result": ""
   }
 
   componentDidMount() {
@@ -17,19 +17,21 @@ class App extends Component {
     });
   }
 
+
   render() {
     return (
+
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          {/* <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2> */}
         </div>
         <p className="App-intro">
           Test value {this.state.test} <br />
           {this.state.result}
         </p>
       </div>
-    );
+    )
   }
 }
 
