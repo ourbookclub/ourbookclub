247f4b192788a879a01fc16e508b397af1eef0c6
Smushed <smushedcode@gmail.com>
2019-02-13 21:30:09 -0600

    Working on deleting comments

--- handlers/postHandler.js
+++ handlers/postHandler.js
@@ -18,7 +18,7 @@ module.exports = {
 
         return addedPost;
     },
-    getGroupPost: async (groupID) => {
+    getAllGroupPost: async (groupID) => {
         const groupPosts = await db.Post.find({ group: groupID });
         return groupPosts;
     },
@@ -27,11 +27,33 @@ module.exports = {
         return sortedPosts;
     },
     createComment: async (userID, postID, comment) => {
+        //We must give comments an ID on our own since they are nested underneath the post
         const commentID = uuidv4();
         const postedComment = await db.Post.findByIdAndUpdate([postID], {
             $push: { comment: { 'user': userID, 'text': comment, 'date': Date.now(), '_id': commentID } }
-        });
+        }, { new: true });
 
         return postedComment
+    },
+    getPostData: async (postID) => {
+        const foundPost = await db.Post.findById([postID]);
+        return foundPost;
+    },
+    getPostByComment: async (commentID) => {
+        console.log(commentID)
+        const foundComment = await db.Post.find({ 'comment._id': commentID });
+        return foundComment;
+    },
+    deletePost: async (userID, userPost, isMod) => {
+        if (isMod || userID === userPost.user) {
+            await db.Post.findByIdAndDelete([userPost._id]);
+            return 200;
+        } else {
+            //Not authorized to delete the post
+            return 500;
+        };
+    },
+    deleteComment: async (commentID) => {
+
     }
 }
