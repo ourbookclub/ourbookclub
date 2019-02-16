9be72e05e0d7444f1156328bf8a55861cb462944
Smushed <smushedcode@gmail.com>
2019-02-16 03:08:43 -0600

    Removing images and tweaking to work with Heroku

---
+++ client/build/precache-manifest.881855eba85aea9ebc5dc573baeb31ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "00cbbe49ad7f039a15f5",
+    "url": "/static/js/main.00cbbe49.chunk.js"
+  },
+  {
+    "revision": "28eb69c25e6b9607ed25",
+    "url": "/static/js/2.28eb69c2.chunk.js"
+  },
+  {
+    "revision": "00cbbe49ad7f039a15f5",
+    "url": "/static/css/main.7701cfa4.chunk.css"
+  },
+  {
+    "revision": "9bf581dc9074681973991bcef95bdb0c",
+    "url": "/index.html"
+  }
+];
